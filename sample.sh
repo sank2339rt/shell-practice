7804985a0f7df32a027a8994a3b58fd574ad34e2
sank2339 <sank2339rt@gmail.com>
2026-05-09 13:06:57 +0530

    sample file tested

--- sample.sh
+++ sample.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-person1="kumar"
-person2="verma"
+person1="$1"
+person2="$2"
 
 echo "$person1 - Hi $person2, I have joined DEVOPS course"
 echo "$person2 - OK.$person1. All the best."
